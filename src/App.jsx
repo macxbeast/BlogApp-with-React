6b7567d9b4e450c7254b741e167c5a3df99d7aaa
macxbeast <de4881mimansa@gmail.com>
2025-08-05 12:45:44 +0530

    authentication and configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
+import conf from './conf/conf'
 import './App.css'
 
 function App() {
-
+  console.log(conf.appwriteUrl)
   return (
     <>
       <h1>Blog with Appwrite</h1>
