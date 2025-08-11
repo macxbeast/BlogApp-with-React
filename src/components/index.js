037b1c9dabf1a3d6157448feecbe4101e616c5e5
macxbeast <de4881mimansa@gmail.com>
2025-08-11 10:08:26 +0530

    login authentication

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,19 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Container from "./container/container";
+import Button from "./Button"
+import Input from "./Input"
+import Select from './Select'
+import PostCard from './PostCard'
 
 export {
     Header,
     Footer,
     Logo,
     LogoutBtn,
-    Container
+    Container,
+    Button,
+    Input,
+    Select,
+    PostCard
 }
