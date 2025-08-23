8067aee45dbd61ca40315b80bb0a04c61e023d63
macxbeast <de4881mimansa@gmail.com>
2025-08-23 14:15:41 +0530

    fixed while deployment

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-import Container from "./container/container";
+import Container from "./container/Container";
 import Button from "./Button"
 import Input from "./Input"
 import Select from './Select'
